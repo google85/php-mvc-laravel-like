a55af35996f5b99971f7c1c11be956588ff4bca0
google85 <bpfcomp2005@gmail.com>
2022-06-13 22:39:42 +0300

    updated password for pdo db

--- src/index.php
+++ src/index.php
@@ -1,7 +1,7 @@
 <?php
 
 try {
-    $pdo = new PDO('mysql:host=127.0.0.1;dbname=djbpfcom_dev_mytodo', 'djbpfcom_dev_1', 'password');
+    $pdo = new PDO('mysql:host=127.0.0.1;dbname=djbpfcom_dev_mytodo', 'djbpfcom_dev_1', 'Password321Password');
 } catch (PDOException $e) {
     die('Could not connect.');
 }
