2b9d071ec09ac61b86fd8416345ce5a51ccb62bb
google85 <bpfcomp2005@gmail.com>
2022-06-13 22:58:45 +0300

    PDO query result in a Task class

--- src/index.php
+++ src/index.php
@@ -1,5 +1,7 @@
 <?php
 
+require 'Task.php';
+
 try {
     $pdo = new PDO('mysql:host=127.0.0.1;dbname=djbpfcom_dev_mytodo', 'djbpfcom_dev_1', 'Password321Password');
 } catch (PDOException $e) {
@@ -10,6 +12,6 @@ $stmt = $pdo->prepare('select * from todos');
 
 $stmt->execute();
 
-$tasks = $stmt->fetchAll(PDO::FETCH_OBJ);
+$tasks = $stmt->fetchAll(PDO::FETCH_OBJ, 'Task');
 
 require 'index.view.php';
--- src/index.view.php
+++ src/index.view.php
@@ -2,7 +2,7 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <title>Document</title>
+    <title>Todos</title>
     <style>
         header {
             background: #e3e3e3;
