64d9e692019c74c2caa66c1071952f23cbe32575
google85 <bpfcomp2005@gmail.com>
2022-06-13 22:27:07 +0300

    small edits

---
+++ src/index.view.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Document</title>
+    <style>
+        header {
+            background: #e3e3e3;
+            padding: 2em;
+            text-align: center;
+        }
+    </style>
+</head>
+<body>
+    
+    <ul>
+        <li>test</li>
+    </ul>
+
+</body>
+</html>
