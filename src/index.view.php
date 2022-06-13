4b5ae3f929fc0cbde89716170d4614e86df1f9a1
google85 <bpfcomp2005@gmail.com>
2022-06-13 22:51:51 +0300

    task foreach in view, from db

--- src/index.view.php
+++ src/index.view.php
@@ -14,7 +14,15 @@
 <body>
     
     <ul>
-        <li>test</li>
+        <?php foreach ($tasks as $task) : ?>
+            <li>
+                <?php if ($task->completed) : ?>
+                    <strike><?= $task->description ?></strike>
+                <?php else: ?>
+                    <?= $task->description ?>
+                <?php endif; ?>
+            </li>
+        <?php endforeach; ?>
     </ul>
 
 </body>
