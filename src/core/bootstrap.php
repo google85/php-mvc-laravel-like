5e00f8a655a9c085c00baa161caa7cbe23440b04
google85 <florin@ineditvision.ro>
2022-06-14 13:25:16 +0300

    refactoring some of bootstrap job into a App class

--- src/core/bootstrap.php
+++ src/core/bootstrap.php
@@ -1,14 +1,23 @@
 <?php
 
-$app = [];
+
 
 if (!file_exists('../config.php')) {
     die('Please create a config file by copying from copy.sample.php!');
 }
 
-$app['config'] = require '../config.php';
+
+App::bind('config', require('../config.php'));
+
+//die(var_dump(App::get('config')));
+
+//$config = App::get('config');
 
 
-$app['database'] = new QueryBuilder(
-    Connection::make($app['config']['database'])
-);
+//$app = [];
+//$app['config'] = require '../config.php';
+//$app['database'] = 
+
+App::bind('database', new QueryBuilder(
+    Connection::make(App::get('config')['database'])
+));
