3df3e1c2814c969d797c217f03aaede6cb83039b
google85 <florin@ineditvision.ro>
2022-06-14 14:33:38 +0300

    fixed config optional path

--- src/index.php
+++ src/index.php
@@ -1,15 +1,15 @@
 <?php
 
-use App\Core\{Router, Request};
+use App\Core\{App, Router, Request};
 
 error_reporting(E_ALL);
 ini_set('display_errors', '1');
 
-require('vendor/autoload.php');
+require 'vendor/autoload.php';
 require 'core/bootstrap.php';
 
 //optional base folder where the project is
-$pathPrefix = $app['config']['optional']['path_prefix'] ?? null;
+$pathPrefix = App::get('config')['optional']['path_prefix'] ?? null;
 
 Router::load('../routes.php')
     ->direct(
